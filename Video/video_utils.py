8385fd941565a18b352e167457236b906ccb3036
ganhao <751123476@qq.com>
2022-12-12 16:49:24 +0800

    添加`ext`参数

--- Video/video_utils.py
+++ Video/video_utils.py
@@ -9,12 +9,13 @@ def decode_fourcc(cc):
     return "".join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
 
 
-def extract_frames(video_path, steps=10):
+def extract_frames(video_path, steps=10, ext='jpg'):
     """
     每{steps}帧提取1帧，并保存在和视频同名的文件夹中。
     Args:
         video_path (str): 带后缀的视频名，如“D:/001.mp4”
-        steps (int): 每{steps}帧提取1帧
+        steps (int): 每{steps}帧提取1帧，默认为10
+        ext (str): 图片后缀，默认为jpg
     """
     # 1. 读取视频
     video_path = Path(video_path)
@@ -42,8 +43,8 @@ def extract_frames(video_path, steps=10):
     # 例如：视频有100帧，即num_frames=100，那么str(num_frames)='100'，
     #      len(str(num_frames))=3，所以需要填充3个0。
     num_0s = len(str(num_frames))
-    # 图片名：视频名_帧索引.jpg
-    save_name = '{}_{:0>{}d}.jpg'
+    # 图片名：视频名_帧索引.ext
+    save_name = '{}_{:0>{}d}.{}'
     for i in trange(num_frames):
         rtn, frame = cap.read()
         if not rtn:
@@ -53,7 +54,7 @@ def extract_frames(video_path, steps=10):
         if i % steps != 0:
             continue
 
-        save_path = save_dir / save_name.format(video_path.stem, i, num_0s)
+        save_path = save_dir / save_name.format(video_path.stem, i, num_0s, ext)
         cv2.imwrite(str(save_path), frame)
 
 
