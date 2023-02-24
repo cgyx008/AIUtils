4ae7613f3730b448cc74f3fb727d01ab014681ac
ganhao <751123476@qq.com>
2023-02-24 10:54:09 +0800

    添加PyTorch转ONNX文件convert.py

---
+++ model/convert.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+import onnx
+import onnxsim
+import torch
+
+
+def load_model(pt_path):
+    """
+    Load PyTorch model.
+    Args:
+        pt_path (str | Path): pt path
+
+    Returns:
+        (torch.nn.Module): model
+    """
+    pt = torch.load(pt_path, map_location='cpu')
+    pt_model = pt['model']
+    return pt_model
+
+
+def pt2onnx(pt_path, onnx_path='', simplify=True, **kwargs):
+    """Convert PyTorch model to simplified ONNX model"""
+    # .pt to .onnx
+    pt_model = load_model(pt_path)
+    dummy_input = torch.randn(1, 3, 224, 224)
+    onnx_path = onnx_path or Path(pt_path).with_suffix('.onnx')
+    torch.onnx.export(pt_model, dummy_input, onnx_path, **kwargs)
+    print(torch.onnx.export_to_pretty_string(pt_model, dummy_input))
+    print('=' * 80)
+
+    # Checks
+    onnx_model = onnx.load(onnx_path)
+    onnx.checker.check_model(onnx_model)
+    # Print a human-readable representation of the graph
+    print(onnx.helper.printable_graph(onnx_model.graph))
+
+    # Simplify
+    if simplify:
+        onnx_model, check = onnxsim.simplify(onnx_model)
+        assert check, 'assert check failed'
+        onnx.save(onnx_model, onnx_path)
+
+
+def main():
+    pt2onnx('E:/data/Projects/mae/mae_pretrain_vit_base.pth')
+
+
+if __name__ == '__main__':
+    main()
