d41be243be116020d50269fb271f544c50481446
ganhao <751123476@qq.com>
2023-09-19 10:46:03 +0800

    提取函数get_cap_and_attr

--- video/video_utils.py
+++ video/video_utils.py
@@ -10,6 +10,32 @@ def decode_fourcc(cc):
     return "".join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
 
 
+def get_cap_and_attr(video_path):
+    """
+    读取视频和属性
+    Args:
+        video_path (str | Path): 视频路径
+
+    Returns:
+        (cv2.VideoCapture, int, int, int, str, str): 视频对象，帧数，帧宽，帧高，帧率，FOURCC
+    """
+    cap = cv2.VideoCapture(str(video_path))
+
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = decode_fourcc(cap.get(cv2.CAP_PROP_FOURCC))
+
+    print(f"{video_path}视频属性：")
+    print(f"帧数：{num_frames}")
+    print(f"宽高：{width}， {height}")
+    print(f"帧率：{fps}")
+    print(f"格式：{fourcc}")
+
+    return cap, num_frames, width, height, fps, fourcc
+
+
 def extract_frames(video_path, steps=10, ext='jpg', max_workers=8):
     """
     每{steps}帧提取1帧，并保存在和视频同名的文件夹中。
@@ -19,31 +45,19 @@ def extract_frames(video_path, steps=10, ext='jpg', max_workers=8):
         ext (str): 图片后缀，默认为jpg
         max_workers (int): 最大线程数
     """
-    # 1. 读取视频
+    # 1. 读取视频和打印属性
     video_path = Path(video_path)
-    cap = cv2.VideoCapture(str(video_path))
+    cap, num_frames = get_cap_and_attr(video_path)[:2]
 
-    # 2. 打印视频属性
-    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    fourcc = decode_fourcc(cap.get(cv2.CAP_PROP_FOURCC))
-    print(f'{video_path}视频属性：')
-    print(f'帧数：{num_frames}')
-    print(f'宽高：{width}， {height}')
-    print(f'帧率：{fps}')
-    print(f'格式：{fourcc}')
-
-    # 3. 新建保存帧的文件夹，与视频同目录
+    # 2. 新建保存帧的文件夹，与视频同目录
     save_dir = video_path.parent / video_path.stem / 'images'
     save_dir.mkdir(exist_ok=True, parents=True)
     print(f'帧保存在文件夹：{save_dir}')
 
-    # 4. 创建线程池
+    # 3. 创建线程池
     executor = ThreadPoolExecutor(max_workers)
 
-    # 5. 提取帧
+    # 4. 提取帧
     # len(str(num_frames))自动计算需要填充多少个0。
     # 例如：视频有100帧，即num_frames=100，那么str(num_frames)='100'，
     #      len(str(num_frames))=3，所以需要填充3个0。
@@ -62,14 +76,13 @@ def extract_frames(video_path, steps=10, ext='jpg', max_workers=8):
         save_path = save_dir / save_name.format(video_path.stem, i, num_0s, ext)
         executor.submit(cv2.imwrite, str(save_path), frame)  # noqa
 
+    executor.shutdown()
+
 
 def rewrite_video():
     video_path = Path(r'G:\Data\AD\reolink\videos\EnWeChat\antelope_fp_person\fn_dog.mp4')
-    cap = cv2.VideoCapture(str(video_path))
-    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
+    cap, num_frames, width, height, fps, _ = get_cap_and_attr(video_path)
+
     save_path = video_path.parent / f'{video_path.stem}_rewrite.mp4'
     vw = cv2.VideoWriter(str(save_path),
                          cv2.VideoWriter_fourcc(*'mp4v'), fps,
