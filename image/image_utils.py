e9646bba1661b7ddd450cc4370d8947e737bb57d
ganhao <751123476@qq.com>
2023-11-02 15:16:13 +0800

    设置cv2画框的线条类型为`cv2.LINE_AA`

--- image/image_utils.py
+++ image/image_utils.py
@@ -6,13 +6,14 @@ from tqdm import trange
 
 
 def draw_rect_and_put_text(img, box, text, color=(0, 0, 255), box_thickness=1,
-                           font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=0.6):
+                           font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=0.6,
+                           line_type=cv2.LINE_AA):
     # bounding-box
     h, w, _ = img.shape
     box = list(map(int, box))
     pt1 = (max(box[0], 0), max(box[1], 0))
     pt2 = (min(box[2], w - 1), min(box[3], h - 1))
-    cv2.rectangle(img, pt1, pt2, color, box_thickness)
+    cv2.rectangle(img, pt1, pt2, color, box_thickness, lineType=line_type)
 
     # text-box
     tbox_color = color
@@ -23,7 +24,7 @@ def draw_rect_and_put_text(img, box, text, color=(0, 0, 255), box_thickness=1,
     else:
         tbox_pt1 = (pt1[0], pt1[1] - text_size[0][1])
         tbox_pt2 = (pt1[0] + text_size[0][0], pt1[1])
-    cv2.rectangle(img, tbox_pt1, tbox_pt2, tbox_color, -1)
+    cv2.rectangle(img, tbox_pt1, tbox_pt2, tbox_color, -1, lineType=line_type)
 
     # text
     if pt1[1] - text_size[0][1] < 0:
@@ -31,7 +32,8 @@ def draw_rect_and_put_text(img, box, text, color=(0, 0, 255), box_thickness=1,
     else:
         text_pt = pt1
     tcolor = (255, 255, 255)
-    cv2.putText(img, text, text_pt, font, font_scale, tcolor, box_thickness * 2)
+    cv2.putText(img, text, text_pt, font, font_scale, tcolor, box_thickness * 2,
+                lineType=line_type)
     return img
 
 
